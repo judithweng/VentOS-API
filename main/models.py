2e95f09cd1e77806589e04d88403f67918acf0ef
Alicia Maydeline <aliciamaydeline@Alicias-MacBook.local>
2021-12-16 17:37:00 -0500

    adding Person model to the database/admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
@@ -14,3 +16,17 @@ class PIRCS(models.Model):
     def __str__(self) -> str:
         return ("com: " + self.com + ", par: " + self.par + ", loc: "
                 + self.int + ", mod: " + str(self.mod) + ", val: " + str(self.val))
+
+
+class Person(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    height = models.FloatField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=1)
+    resistance = models.FloatField()
+    compliance = models.FloatField()
+
+    def __str__(self) -> str:
+        return ("id: " + str(self.id) + "\nheight: " + str(self.height) +
+                " cm, weight: " + str(self.weight) + " kg, sex: " + self.sex +
+                "\nresistance: " + str(self.resistance) + ", compliance: " + str(self.compliance))

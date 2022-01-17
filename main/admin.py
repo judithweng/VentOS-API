02afba8e4bf2805c8146b16332817b08bcb34d32
Alicia Maydeline <aliciamaydeline@Alicias-MacBook.local>
2022-01-17 18:48:56 -0500

    fix database models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PIRCS, PatientState, VentilatorState, Session, Person
+from .models import PIRCS, PatientState, VentilatorState, Person, Session
 # Register your models here.
 admin.site.register(PIRCS)
 admin.site.register(Person)
--- main/models.py
+++ main/models.py
@@ -6,15 +6,40 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 
+class Person(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    height = models.FloatField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=1)
+    resistance = models.FloatField()
+    compliance = models.FloatField()
+
+    def __str__(self) -> str:
+        return ("id: " + str(self.id) + ", name: " + self.name + ", height: " + str(self.height) +
+                " cm, weight: " + str(self.weight) + " kg, sex: " + self.sex +
+                ", resistance: " + str(self.resistance) + ", compliance: " + str(self.compliance))
+
+
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return ("session id: " + str(self.id))
+
+
 class PIRCS(models.Model):
     com = models.CharField(max_length=1)
     par = models.CharField(max_length=1)
     int = models.CharField(max_length=1)
     mod = models.IntegerField()
     val = models.IntegerField()
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, default=None)
 
     def __str__(self) -> str:
-        return ("com: " + self.com + ", par: " + self.par + ", loc: "
+        return ("sid: " + str(self.session) + ", com: " + self.com + ", par: " + self.par + ", loc: "
                 + self.int + ", mod: " + str(self.mod) + ", val: " + str(self.val))
 
 
@@ -28,12 +53,14 @@ class PatientState(models.Model):
     pressure_intrapleural = models.FloatField()
     flow = models.FloatField()
     log = models.JSONField()
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, default=None)
 
     def __str__(self) -> str:
         if len(self.log) == 0:
-            return ("time: " + str(self.timestamp) + " - log is not yet available")
+            return ("sid: " + str(self.session) + ", time: " + str(self.timestamp) + " - log is not yet available")
         else:
-            return ("time: " + str(self.timestamp) + " - last logged patient state: " + str(self.log[-1]))
+            return ("sid: " + str(self.session) + ", time: " + str(self.timestamp) + " - last logged patient state: " + str(self.log[-1]))
 
 
 class VentilatorState(models.Model):
@@ -47,36 +74,11 @@ class VentilatorState(models.Model):
     IE = models.FloatField()
     phase = models.CharField(max_length=1)
     log = models.JSONField()
+    session = models.ForeignKey(
+        Session, on_delete=models.CASCADE, default=None)
 
     def __str__(self) -> str:
         if len(self.log) == 0:
-            return ("time: " + str(self.timestamp) + " - log is not yet available")
+            return ("sid: " + str(self.session) + ", time: " + str(self.timestamp) + " - log is not yet available")
         else:
-            return ("time: " + str(self.timestamp) + " - last logged ventilator state: " + str(self.log[-1]))
-
-
-class Session(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    pircs = models.ForeignKey(PIRCS, on_delete=models.CASCADE)
-    patientState = models.ForeignKey(PatientState, on_delete=models.CASCADE)
-    ventilatorState = models.ForeignKey(
-        VentilatorState, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return ("session id: " + str(self.id))
-
-
-class Person(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50)
-    height = models.FloatField()
-    weight = models.FloatField()
-    sex = models.CharField(max_length=1)
-    resistance = models.FloatField()
-    compliance = models.FloatField()
-    session = models.OneToOneField(Session, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        return ("id: " + str(self.id) + ", name: " + self.name + ", height: " + str(self.height) +
-                " cm, weight: " + str(self.weight) + " kg, sex: " + self.sex +
-                ", resistance: " + str(self.resistance) + ", compliance: " + str(self.compliance))
+            return ("sid: " + str(self.session) + ", time: " + str(self.timestamp) + " - last logged ventilator state: " + str(self.log[-1]))

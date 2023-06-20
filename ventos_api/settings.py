aed94a55bac423daf355d7b1d65ed40554d34dcb
judithweng <wengjudith@gmail.com>
2023-06-20 17:37:20 -0400

    edit settings.py to import database-url

--- ventos_api/settings.py
+++ ventos_api/settings.py
@@ -16,6 +16,8 @@ Reference: https://github.com/heroku/python-getting-started/blob/main/gettingsta
 import os
 from pathlib import Path
 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

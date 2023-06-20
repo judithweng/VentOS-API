60843260122be80195b1036a6cfedf4120feed15
judithweng <wengjudith@gmail.com>
2023-06-20 17:29:46 -0400

    edit settings.py to import os

--- ventos_api/settings.py
+++ ventos_api/settings.py
@@ -13,7 +13,9 @@ Reference: https://github.com/heroku/python-getting-started/blob/main/gettingsta
 
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
